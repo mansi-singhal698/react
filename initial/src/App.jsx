c2e68ab8958af8b08dc145cc4d84bdfc3a6c01d5
Mansi Singhal <singhalmansi817@gmail.com>
2025-09-01 23:41:46 +0530

    add files

--- initial/src/App.jsx
+++ initial/src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-  
-      <h1>hello</h1>
+import Login,{Profile} from './importComponent'
+function App(){
+  return(
+    <div>
+      <h1>First component</h1>
+      <Fruit></Fruit>
+      <Login></Login>
+      <Profile />
+    </div>
+    
   )
 }
 
-export default App
+
+function Fruit(){
+  return(
+    <h1>Apple</h1>
+  )
+}
+export default App;
---
+++ initial/src/importComponent.jsx
@@ -0,0 +1,17 @@
+function Login(){
+    return(
+        <div>
+            <h1>Login User</h1>
+        </div>
+    )
+}
+
+export function Profile(){
+    return(
+        <div>
+            <h1>Profile</h1>
+        </div>
+    )
+}
+
+export default Login;
